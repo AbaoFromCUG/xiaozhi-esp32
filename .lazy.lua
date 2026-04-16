72bf147cfe9bb391bac1773b6615eb796d3a1b27
Abao Zhang <abaodoit@gmail.com>
2026-04-16 21:27:33 +0800

    feat: support multiple wake words

--- .lazy.lua
+++ .lazy.lua
@@ -3,13 +3,17 @@ vim.api.nvim_create_autocmd("User", {
     pattern = "VeryLazy",
     callback = function()
         vim.lsp.config("clangd", require("esp32").lsp_config())
+        vim.lsp.enable("clangd")
     end,
 })
 
 return {
     {
         "Aietes/esp32.nvim",
-        opts = {},
+        lazy = false,
+        opts = {
+            build_dir = "build",
+        },
         keys = {
             {
                 "<leader>,e",
